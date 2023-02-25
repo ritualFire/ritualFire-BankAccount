ef7cf1563f6d0dc9fc9e175db68b0a491a2476ba
ritualFire <jpelaez456@gmail.com>
2023-02-25 17:46:11 +0000

    Bank Account

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -1,3 +1,5 @@
+import static org.junit.Assume.assumeNotNull;
+
 /**
  * This service class is intended to contain the basic logic for deposits and withdrawls to a bank account
  * Separating menu logic and bank logic is good for two reasons: it makes both classes as simple
@@ -23,6 +25,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        this.balance += amount;
 
     }
 
@@ -32,6 +35,10 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        if(this.balance >= amount)
+        {
+            this.balance -= amount;
+        }
 
     }
 
@@ -40,6 +47,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
